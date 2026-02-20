0d5a68e9d27f2169ab8905294fe3d2f75cbdcae9
bisinella <eduardo.bisinella@rcxit.com.br>
2026-02-20 17:46:24 -0300

    feat: adiciona rotas da API para sincronização e listagem

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SincronizacaoController;
+
+Route::post('/sincronizar/produtos', [SincronizacaoController::class, 'sincronizarProdutos']);
+Route::post('/sincronizar/precos', [SincronizacaoController::class, 'sincronizarPrecos']);
+Route::get('/produtos-precos', [SincronizacaoController::class, 'listar']);
